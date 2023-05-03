3a3f00f07776f55cd09f19f32febd1249efba7e6
Viorel Moldovan <viorelmoldovan@gmail.com>
2023-05-03 19:33:19 +0300

    updated hour input on booking component

--- src/components/booking.jsx
+++ src/components/booking.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 const BookingContainer = styled.div.attrs({
@@ -47,10 +47,9 @@ const Button = styled.button`
     }
     `;
 
-
 const CalendarWrapper = styled.div`
-margin-top: 50px;
-    padding: 0.75rem;
+    margin-top: 50px;
+    padding: 0rem;
     border-radius: 0.5rem;
     background-color: #f9fafb;
     display: flex;
@@ -59,149 +58,152 @@ margin-top: 50px;
     `;
 
 const CalendarButton = styled.button`
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
+    width: 4rem;
+    height: 1.5rem;
+    border-radius: 0%;
     font-weight: bold;
     font-size: 0.875rem;
     outline: none;
     `;
 
-const SliderWrapper = styled.div`
-    margin-top: 50px;
+const RangeInputWrapper = styled.div`
     width: 50%;
+    margin-top: 50px;
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-  `;
-  
-const TimeBar = styled.div.attrs({
-    id: 'time-bar'
-})`
-    width: 80%;
-    height: 10px;
-    margin: 0 10px;
-    background-color: #ccc;
+    flex-direction: column;
+`;
+
+const RangeInputLabel = styled.label`
+    opacity: 0;
+`;
+
+const RangeInput = styled.input.attrs(props => ({
+    type: 'range',
+    min: '9',
+    max: '16.5',
+    step: '0.5',
+    value: props.value
+  }))`
+  -webkit-appearance: none;
+  width: 100%;
+  height: 16px;
+  background-color: #E9EFF4;
+  border-radius: 0px;
+  outline: none;
+  margin-bottom: 16px;
+
+  &::-moz-range-thumb {
+    width: 48px;
+    height: 16px;
+    background-color: #fafafa;
+    border-radius: 0%;
+    cursor: pointer;
+  }
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 16px;
+    width: 48px;
+    background-color: #fafafa;
+    border-radius: 0%;
+    cursor: pointer;
+  }
+`;
+const RangeInputValue = styled.span`
+    display: inline-block;
     position: relative;
-  `;
-  
-const SliderButton = styled.button`
-    width: 80px;
-    height: 30px;
-    background-color: #fff;
-    border: 2px solid #ccc;
-    position: relative;
-    transform: translate(0%, -35%);
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    width: 40px;
+    height: 20px;
+    border: 1px solid #ccc;
+    border-radius: 0px;
+    text-align: center;
+    line-height: 25px;
     font-size: 14px;
     font-weight: bold;
-    cursor: grab;
-    outline: none;
-    &:active {
-      cursor: grabbing;
-    }
-  `;
-  
-const TimeInput = styled.input`
-    opacity: 1;
-    width: 120px;
-    font-size: 14px;
-    text-align: center;
-  `;    
+    background-color: #fff;
+    color: #000;
+    margin-left: 5px;
+    margin-bottom: 3px;
 
-
+    &::after {
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #ccc transparent transparent transparent;
+  }
+`;
 
 function AppointmentBooking() {
-    const [sliderPosition, setSliderPosition] = useState(0);
-    //
+    const inputRef = useRef(null);
+    const [inputWidth, setInputWidth] = useState(0);
+    const [value, setValue] = useState(9);
     const [service, setService] = useState('');
     const [date, setDate] = useState('');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
 
-function handleSubmit(event) {
-        event.preventDefault();
-        console.log("appointment confirmed")    
+    useEffect(() => {
+        if (inputRef.current) {
+          setInputWidth(inputRef.current.offsetWidth);
+        }
+    }, []);
+
+    const handleRangeChange = (event) => {
+        setValue(parseFloat(event.target.value));
+    }
+
+    function handleSubmit(event) {
+            event.preventDefault();
+            console.log("appointment confirmed")    
     }
 
 
-const handleSliderMove = (e) => {
-        const timeBar = document.getElementById("time-bar");
-        const timeBarWidth = timeBar.clientWidth;
-        const sliderWidth = e.target.clientWidth;
-        const newPosition = e.clientX - timeBar.getBoundingClientRect().left - sliderWidth / 2;
-        const maxPosition = timeBarWidth - sliderWidth;
-        const snapValue = Math.round(newPosition / (maxPosition / 8));
-        setSliderPosition(Math.min(maxPosition, Math.max(0, snapValue * (maxPosition / 8))));
-        
-        console.log("maxPosition: ", maxPosition)
-        console.log("snapValue: ", snapValue)
-        console.log("newPosition: ", newPosition)
-        console.log("sliderposition: ", sliderPosition)
-    };
-  
-const getTimeFromPosition = (position) => {
-      const hour = Math.floor((position / 480) * 8) + 9;
-      const minute = position % 60 === 0 ? "00" : "30";
-      const time = `${hour}:${minute}`;
-      console.log(">>>", position)
-      return time;
-    };
-  
-const handleTimeChange = (e) => {
-      const newTime = e.target.value;
-      console.log("newtime", newTime)
-      const hour = parseInt(newTime.split(":")[0]);
-      const minute = parseInt(newTime.split(":")[1]);
-      const newPosition = ((hour - 9) * 60 + minute) * 6;
-      setSliderPosition(newPosition);
-    };
+    return (
+        <BookingContainer>
+            <Form onSubmit={handleSubmit}>
+                
+                <Label>
+                    Select a Service:
+                    <Select value={service} onChange={(e) => setService(e.target.value)}>
+                        <option value="">-- Please select --</option>
+                        <option value="Oil Change">Oil Change</option>
+                        <option value="Tire Rotation">Tire Rotation</option>
+                        <option value="Brake Repair">Brake Repair</option>
+                    </Select>
+                </Label>
+                
+                <CalendarWrapper>
+                    <CalendarButton active>Today</CalendarButton>
+                    <CalendarButton>27</CalendarButton>
+                    <CalendarButton active>28</CalendarButton>
+                    <CalendarButton active>29</CalendarButton>
+                    <CalendarButton active>30</CalendarButton>
+                    <CalendarButton active>31</CalendarButton>
+                </CalendarWrapper>
 
+                <RangeInputWrapper>
+                    <RangeInputLabel htmlFor="hour-select"> - {value} - </RangeInputLabel>
+                    <RangeInputValue
+                        style={{ left: `calc(${(value-9) * 12}%)` }}
+                        >
+                        {value}~{inputWidth}
+                    </RangeInputValue>
+                    <RangeInput 
+                        ref={inputRef}
+                        id="hour-select" 
+                        name="hour-select" 
+                        value={value}
+                        onChange={handleRangeChange}
+                    />
+                    
+                </RangeInputWrapper>
 
-return (
-    <BookingContainer>
-        <Form onSubmit={handleSubmit}>
-            
-            <Label>
-                Select a Service:
-                <Select value={service} onChange={(e) => setService(e.target.value)}>
-                    <option value="">-- Please select --</option>
-                    <option value="Oil Change">Oil Change</option>
-                    <option value="Tire Rotation">Tire Rotation</option>
-                    <option value="Brake Repair">Brake Repair</option>
-                    {/* Add additional options for other services provided */}
-                </Select>
-            </Label>
-            
-            <CalendarWrapper>
-                <CalendarButton active>26</CalendarButton>
-                <CalendarButton>27</CalendarButton>
-                <CalendarButton active>28</CalendarButton>
-                <CalendarButton active>29</CalendarButton>
-                <CalendarButton active>30</CalendarButton>
-                <CalendarButton active>31</CalendarButton>
-            </CalendarWrapper>
-
-            <SliderWrapper>
-                <TimeInput type="time" defaultValue="09:00" onChange={handleTimeChange} />
-                <TimeBar id="time-bar">
-                    <SliderButton style={{left: sliderPosition}} 
-                        onMouseDown={handleSliderMove} 
-                        onMouseUp={() => document.removeEventListener("mousemove", handleSliderMove)}>
-                        {getTimeFromPosition(sliderPosition)}
-                    </SliderButton>
-                </TimeBar>
-                <TimeInput type="time" defaultValue="17:00" disabled />
-            </SliderWrapper>
-
-
-
-
-        </Form>
-    </BookingContainer>
+            </Form>
+        </BookingContainer>
     );
 }
 
@@ -210,25 +212,29 @@ export default AppointmentBooking;
 
 
 /*
-            <Label>
-            Select a Date:
-            <Input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-            </Label>
-            <Label>
-            Select a Time:
-            <Input type="time" value={time} onChange={(e) => setTime(e.target.value)} />
-            </Label>
-            <Label>
-            Name:
-            <Input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            </Label>
-            <Label>
-            Email:
-            <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </Label>
-            <Label>
-            Phone Number:
-            <Input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
-            </Label>
-            <Button type="submit">Book Appointment</Button>
+
+
+<Label>
+Select a Date:
+<Input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+</Label>
+<Label>
+Select a Time:
+<Input type="time" value={time} onChange={(e) => setTime(e.target.value)} />
+</Label>
+<Label>
+Name:
+<Input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+</Label>
+<Label>
+Email:
+<Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+</Label>
+<Label>
+Phone Number:
+<Input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
+</Label>
+<Button type="submit">Book Appointment</Button>
+
+
 */
